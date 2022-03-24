82016f06f5f96cdee4aa440b044681e999dcc115
markishere16 <markjoshua.mandigma@gmail.com>
2022-03-25 01:23:01 +0800

    update

--- resources/js/components/pages/login/login.vue
+++ resources/js/components/pages/login/login.vue
@@ -49,7 +49,7 @@ export default {
                 }else{
                     this.loading = false;
                 }
-            }).cathc(()=>{
+            }).catch(()=>{
                 this.loading = false;
             })
         }
